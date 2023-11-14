44e922cc0da787d47e10f5145d9b0644f89dcf40
David Okuonghae <137172804+Okuonghae17@users.noreply.github.com>
2023-11-14 15:08:27 +0100

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Your email address
+    $to = "dokuonghae256@gmail.com"; // Replace with your actual email address
+
+    // Email subject
+    $subject = "New Message from $name";
+
+    // Email headers
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    // Send the email
+    mail($to, $subject, $message, $headers);
+
+    // Redirect the user after sending the email (you can customize this URL)
+    header("Location: thank_you.html");
+} else {
+    // Redirect if accessed directly without form submission
+    header("Location: index.html");
+}
+?>
